222fad1dcad7f3542955336929934a324a2e5116
JJ <jadeandpearls@gmail.com>
2024-07-27 18:28:04 -0700

    some things are working when i use image shortcode

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -17,11 +17,25 @@ const eleventyImage = require("@11ty/eleventy-img");
 // 	}
 // }
 
+
+// Output Widths
+// Controls how many output images will be created for each image format. Aspect ratio is preserved.
+// widths: ["auto"] (default, keep original width) "auto".
+// widths: [200] (output one 200px width)
+// widths: [200, "auto"] (output 200px and original width)
+
+// Output Formats // full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+// Use almost any combination of these:
+// formats: ["webp", "jpeg"] (default)
+// formats: ["png"]
+// formats: ["auto"] (keep original format) "auto"
+// formats: ["svg"] (requires SVG input)
+// formats: ["avif"]  warning: Avif can be resource-intensive so take care!
+
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
-		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
-		// Warning: Avif can be resource-intensive so take care!
-		let formats = ["webp", "auto"];
+		let formats = ["webp", "auto", "svg"];
 		// let input;
 		// if(isFullUrl(src)) {
 		// 	input = src;
@@ -35,7 +49,6 @@ module.exports = function (eleventyConfig) {
 			outputDir: './_site/img/' // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
 		});
 
-		// TODO loading=eager and fetchpriority=high
 		let imageAttributes = {
 			alt,
 			sizes,
